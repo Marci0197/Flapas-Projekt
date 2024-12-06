bc144a088a6e4bae02662458e942690b9187379e
Marcel <140978148+Marci0197@users.noreply.github.com>
2024-12-06 17:12:11 +0100

    Delete login directory

--- login/script.js
+++
@@ -1,9 +0,0 @@
-function showSignup(){
-    document.querySelector('.login-container').style.display = "none";
-    document.querySelector('.signup-container').style.display = "block";
-}
-
-function showLogin(){
-    document.querySelector('.login-container').style.display = "block";
-    document.querySelector('.signup-container').style.display = "none";
-}
