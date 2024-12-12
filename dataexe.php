c968f651eb4f21a3c851357ed9777d3a5f0b5407
Marcel <140978148+Marci0197@users.noreply.github.com>
2024-12-12 15:13:25 +0100

    Update dataexe.php

--- dataexe.php
+++ dataexe.php
@@ -5,12 +5,12 @@ $password="MM-06102009-mm"; //your password for your lower privileged user
 $database="studyzone";  //your database
 
 $conn = mysqli_connect("localhost", $username, $password, $database);
-if (mysqli_connect_errno()) {
+if (mysqli_connect_errno()){
         echo "Failed to connect to database: " . mysqli_connect_error();
         exit();
 }else{
     echo "Connected to database";
-    $sql = "SELECT * FROM Benutzer;";
+    $sql = "SELECT ID, Name FROM Benutzer;";
     $result = mysqli_query($conn, $sql);
     echo "result" + $result;
 }
