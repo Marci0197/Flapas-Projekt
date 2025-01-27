50607fc6381837d35d261adc0f25b95dd3ec25af
Marcel <140978148+Marci0197@users.noreply.github.com>
2025-01-27 15:57:59 +0100

    Add files via upload

--- php/config.php
+++ php/config.php
@@ -1,3 +1,3 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "studyzone") or die("Datenbank konnte sich nicht verbinden");
+$con = mysqli_connect("localhost", "root", "", "studyzone") or die("Datenbank konnte sich nicht verbinden!");
 ?>
