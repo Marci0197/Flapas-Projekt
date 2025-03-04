8b020f907e523fe34865fe96b5020811cf5b37ef
Marcel <140978148+Marci0197@users.noreply.github.com>
2025-03-04 17:05:47 +0100

    Delete test.sql

--- test.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE student (
-    id INT PRIMARY KEY,
-    Na_Ne VARCHAR(100),
-    klasse VARCHAR(50)
-);
-
-INSERT INTO student (id, Na_Ne, klasse) VALUES (1, 'Max Mustermann', '10A');
-
-SELECT * FROM student;
